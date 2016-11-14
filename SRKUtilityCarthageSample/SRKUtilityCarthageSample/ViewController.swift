8cd463626c698b20cdd6937725771b980364facc
Sagar Kothari <skothari@altimetrik.com>
2016-11-14 19:17:37 +0530

    Added carthage sample

---
+++ SRKUtilityCarthageSample/SRKUtilityCarthageSample/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SRKUtilityCarthageSample
+//
+//  Created by Sagar on 11/14/16.
+//  Copyright © 2016 Sagar R. Kothari. All rights reserved.
+//
+
+import UIKit
+import SRKUtilityCarthage
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		let img = UIImage()
+		img.resizeWith(width: 300)
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
