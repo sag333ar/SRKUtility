28cf1dd5b7cbe056d0f868ddedc944ac52a8e48d
Sagar Kothari <skothari@altimetrik.com>
2016-12-12 14:21:39 +0530

    Request manager changes for caching

--- SRKUtility-Source/RequestManager/RequestManager.swift
+++ SRKUtility-Source/RequestManager/RequestManager.swift
@@ -290,4 +290,60 @@ public enum RMResponse {
 		return task
 	}
 	
+	open class func generatedLocalURL(_ urlString: String) -> String {
+		var anotherStr = self.removeURLEncoding(urlString).replacingOccurrences(of: ":", with: "_")
+		anotherStr = anotherStr.replacingOccurrences(of: "/", with: "_")
+		anotherStr = anotherStr.replacingOccurrences(of: "\\", with: "_")
+		anotherStr = anotherStr.replacingOccurrences(of: "%", with: "_")
+		return anotherStr
+	}
+	
+	open class func writeFile(_ tempFilePathURL: URL, fileName: String) throws {
+		let fm = FileManager.default
+		let docDir = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]
+		let filePath = docDir + "/" + fileName
+		let docDirFileURL = URL(fileURLWithPath: filePath)
+		do {
+			try fm.copyItem(at: tempFilePathURL, to: docDirFileURL)
+		} catch {
+			throw error
+		}
+	}
+	
+	@discardableResult open class func downloadFile(_ urlString: String, handler: @escaping (String) -> Void) -> URLSessionDownloadTask? {
+		if let url = URL(string: urlString) {
+			let downloadtask = URLSession.shared.downloadTask(with: url) { (location: URL?, response: URLResponse?, error: Error?) -> Void in
+				if location != nil {
+					print("Local file url is \(location)")
+					let anotherStr = self.generatedLocalURL(urlString)
+					do {
+						try self.writeFile(location!, fileName: anotherStr)
+						handler(anotherStr)
+					} catch {
+						handler("")
+					}
+				}
+			}
+			downloadtask.resume()
+			return downloadtask
+		} else {
+			return nil
+		}
+	}
+	
+	open class func isFileDownloaded(_ urlString: String) -> Bool {
+		let anotherStr = self.generatedLocalURL(urlString)
+		let fm = FileManager.default
+		let docDir = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]
+		let filePath = docDir + "/" + anotherStr
+		return fm.fileExists(atPath: filePath)
+	}
+	
+	open class func loadCachedFile(_ forURLString: String, handler: @escaping (String) -> Void) {
+		if self.isFileDownloaded(forURLString) {
+			handler(self.generatedLocalURL(forURLString))
+		} else {
+			self.downloadFile(forURLString, handler: handler)
+		}
+	}
 }
