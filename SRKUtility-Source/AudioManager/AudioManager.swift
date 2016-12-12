c4c3aff8d18f65a7d2e0dd1761eabba3f047208a
Sagar Kothari <skothari@altimetrik.com>
2016-12-13 01:54:50 +0530

    Added Audio Manager

--- SRKUtility-Source/AudioManager/AudioManager.swift
+++ SRKUtility-Source/AudioManager/AudioManager.swift
@@ -1,11 +1,3 @@
-//
-//  AudioManager.swift
-//  AudioPlayerDemo
-//
-//  Created by sagar kothari on 13/12/16.
-//  Copyright © 2016 Altimetrik India Pvt. Ltd. All rights reserved.
-//
-
 import Foundation
 import UIKit
 import MediaPlayer
@@ -124,7 +116,7 @@ class CustomAudioPlayer: NSObject {
         }
         let mpic = MPNowPlayingInfoCenter.default()
         mpic.nowPlayingInfo = nil
-        self.downloadTask = RequestManager.loadCachedFile(urlString, timeout: timeOut) { (filePath: String) -> Void in
+        self.downloadTask = CustomAudioPlayer.loadCachedFile(urlString, timeout: timeOut) { (filePath: String) -> Void in
             OperationQueue.main.addOperation {
                 if filePath.characters.count > 0 {
                     let docDir = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]
@@ -137,6 +129,72 @@ class CustomAudioPlayer: NSObject {
         }
         if let task = self.downloadTask {
             task.priority = 1.0
+        }
+    }
+}
+
+extension CustomAudioPlayer {
+    
+    class func removeURLEncoding(_ string: String) -> String {
+        return string.removingPercentEncoding!
+    }
+    
+    class func generatedLocalURL(_ urlString: String) -> String {
+        var anotherStr = CustomAudioPlayer.removeURLEncoding(urlString).replacingOccurrences(of: ":", with: "_")
+        anotherStr = anotherStr.replacingOccurrences(of: "/", with: "_")
+        anotherStr = anotherStr.replacingOccurrences(of: "\\", with: "_")
+        anotherStr = anotherStr.replacingOccurrences(of: "%", with: "_")
+        return anotherStr
+    }
+    
+    class func writeFile(_ tempFilePathURL: URL, fileName: String) throws {
+        let fm = FileManager.default
+        let docDir = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]
+        let filePath = docDir + "/" + fileName
+        let docDirFileURL = URL(fileURLWithPath: filePath)
+        do {
+            try fm.copyItem(at: tempFilePathURL, to: docDirFileURL)
+        } catch {
+            throw error
+        }
+    }
+    
+    @discardableResult class func downloadFile(_ urlString: String, timeout: Int, handler: @escaping (String) -> Void) -> URLSessionDownloadTask? {
+        if let url = URL(string: urlString) {
+            let rqst = URLRequest(url: url, cachePolicy: URLRequest.CachePolicy.returnCacheDataElseLoad, timeoutInterval: TimeInterval(timeout))
+            let downloadtask = URLSession.shared.downloadTask(with: rqst) { (location: URL?, response: URLResponse?, error: Error?) -> Void in
+                if location != nil {
+                    print("Local file url is \(location)")
+                    let anotherStr = self.generatedLocalURL(urlString)
+                    do {
+                        try self.writeFile(location!, fileName: anotherStr)
+                        handler(anotherStr)
+                    } catch {
+                        handler("")
+                    }
+                }
+            }
+            downloadtask.resume()
+            return downloadtask
+        } else {
+            return nil
+        }
+    }
+    
+    class func isFileDownloaded(_ urlString: String) -> Bool {
+        let anotherStr = CustomAudioPlayer.generatedLocalURL(urlString)
+        let fm = FileManager.default
+        let docDir = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]
+        let filePath = docDir + "/" + anotherStr
+        return fm.fileExists(atPath: filePath)
+    }
+    
+    class func loadCachedFile(_ forURLString: String, timeout: Int, handler: @escaping (String) -> Void) -> URLSessionDownloadTask? {
+        if CustomAudioPlayer.isFileDownloaded(forURLString) {
+            handler(CustomAudioPlayer.generatedLocalURL(forURLString))
+            return nil
+        } else {
+            return CustomAudioPlayer.downloadFile(forURLString, timeout: timeout, handler: handler)
         }
     }
 }
